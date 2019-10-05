dd2b6330a9280134a6d43f7b9d54db333368dc23
Albert Salim <albertlimca@gmail.com>
2019-10-05 16:43:37 +0800

    kuns - Search and use kubectl namespace

--- conf.d/fizzy-kubectl.fish
+++ conf.d/fizzy-kubectl.fish
@@ -4,7 +4,17 @@ end
 
 if test ! -z $FIZZ_KUC_CMD
   function $FIZZ_KUC_CMD -d "Search and use kubectl context"
-    __fizz_kubectl_use_context $argv
+    __fizz_kubectl_use_context
+  end
+end
+
+if test -z "$FIZZ_KUNS_CMD"
+  set -U FIZZ_KUNS_CMD "kuns"
+end
+
+if test ! -z $FIZZ_KUNS_CMD
+  function $FIZZ_KUNS_CMD -d "Search and use kubectl namespace"
+    __fizz_kubectl_use_namespace
   end
 end
 
